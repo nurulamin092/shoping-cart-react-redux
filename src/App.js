de5128fc7a1737ab014364fb45386d84ccc45f38
nurulamin092 <mnurulamin092@gmial.com>
2023-01-17 08:21:55 +0600

    component is ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import Cart from './components/Cart';
+import Shop from './components/shop/Shop';
 
 function App() {
   return (
-    <div className="bg-gray-50 h-full md:h-screen">
-    <div className="grid place-items-center">
-        <h1
-            className="text-gray-900 font-bold text-3xl p-10 underline decoration-purple-500 decoration-4 underline-offset-8 mb-4"
-        >
-            Shopping Cart
-        </h1>
-    </div>
-  <Cart/>
-</div>
+    <div className="App">
+       <Shop/>
+  </div>
   );
 }
 
